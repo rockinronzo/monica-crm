4f7b54292b9f344c056367fdf15394b851ea5162
Ron Stevens <Ronnie.stevens@hotmail.com>
2026-04-16 18:08:02 -0400

    fix: configure trusted proxies for reverse proxy setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\EnsureSignupIsEnabled;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use Laravel\Sanctum\Http\Middleware\CheckAbilities;
 use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 use LaravelWebauthn\Http\Middleware\WebauthnMiddleware;
@@ -17,6 +18,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_PREFIX
+        );
         $middleware->throttleApi();
         $middleware->validateCsrfTokens(except: [
             '/dav',
--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 return [
 
     /*
@@ -17,5 +19,21 @@ return [
     */
 
     'proxies' => env('APP_TRUSTED_PROXIES', null),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted headers
+    |--------------------------------------------------------------------------
+    |
+    | Which headers to use when determining proxy-forwarded information.
+    | Caddy v2 sends the standard X-Forwarded-* headers by default.
+    |
+    */
+
+    'headers' => Request::HEADER_X_FORWARDED_FOR
+        | Request::HEADER_X_FORWARDED_HOST
+        | Request::HEADER_X_FORWARDED_PORT
+        | Request::HEADER_X_FORWARDED_PROTO
+        | Request::HEADER_X_FORWARDED_PREFIX,
 
 ];
